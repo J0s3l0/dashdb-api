15990b557d7a6f5a721fa734d745001494725556
José Luis García <jgarcia@nearshoremx.com>
2017-05-11 04:06:53 -0500

    + json queries

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var http = require('http');
 var path = require('path');
 var ibmdb = require('ibm_db');
 require('cf-deployment-tracker-client').track();
-var queries = require('./queries');
+var queries = require('./json/queries.json');
 var cors = require('cors');
 var app = express();
 
@@ -70,169 +70,25 @@ if ( hasConnect == false ) {
 
 var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
 
-var query = "SELECT CUSTOMER, COUNT(*) AS TOTAL FROM DASH14467.BLADE_DAMAGE GROUP BY CUSTOMER";
+//app.get('/', routes.listSysTables(ibmdb,connString));
 
-app.get('/', routes.listSysTables(ibmdb,connString));
-app.get('/customers', routes.apicall(ibmdb,connString, queries.engines.customers));
-
-/*app.get('/', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+app.get('/', function (req, res, next) {
+  res.json({api_version: '1.0'});
 })
-*/
 
-app.get('/api/v1/engines/customers', function(req, res, next){
-  ibmdb.open(connString, function(err, conn){
-    if(err){
-      console.error("Error: ", err);
-      return;
-    }else{
-      var query = "SELECT CUSTOMER, COUNT(*) AS TOTAL FROM DASH14467.BLADE_DAMAGE GROUP BY CUSTOMER";
-      conn.query(query, function(err, rows){
-        if(err){
-          console.log("Error: ", err);
-          return;
-        }else {
-          res.send(rows);
-          conn.close(function(){
-            console.log("Connection closed successfully");
-          });
-        }
-      });
-    }
-  });
-});
+app.get('/api/v1/engines/customers', routes.apicall(ibmdb,connString, queries.engines.customers));
 
-app.get('/api/v1/engines/damage_per_flight', function(req, res, next){
-  ibmdb.open(connString, function(err, conn){
-    if(err){
-      console.error("Error: ", err);
-      return;
-    }else{
-      var query = "SELECT FLIGHT_ID, SUM(DAMAGE) AS TOTAL_DAMAGE FROM DASH14467.BLADE_DAMAGE GROUP BY FLIGHT_ID";
-      conn.query(query, function(err, rows){
-        if(err){
-          console.log("Error: ", err);
-          return;
-        }else {
-          res.send(rows);
-          conn.close(function(){
-            console.log("Connection closed successfully");
-          });
-        }
-      });
-    }
-  });
-});
+app.get('/api/v1/engines/damage_per_flight', routes.apicall(ibmdb,connString, queries.engines.damage_per_flight));
 
-app.get('/api/v1/engines/ex50b/core_speed', function(req, res, next){
-  ibmdb.open(connString, function(err, conn){
-    if(err){
-      console.error("Error: ", err);
-      return;
-    }else{
-      var query = "SELECT CUSTOMER, FLIGHT_ID, FAILED, CORE_SPEED FROM DASH14467.BLADE_DAMAGE WHERE ENGINE_TYPEB=2 ORDER BY CORE_SPEED DESC FETCH FIRST 100 ROW ONLY";
-      conn.query(query, function(err, rows){
-        if(err){
-          console.log("Error: ", err);
-          return;
-        }else {
-          res.send(rows);
-          conn.close(function(){
-            console.log("Connection closed successfully");
-          });
-        }
-      });
-    }
-  });
-});
+app.get('/api/v1/engines/ex50b/core_speed', routes.apicall(ibmdb,connString, queries.engines.core_speed));
 
-app.get('/api/v1/prosa/comercios', function(req, res, next){
-  ibmdb.open(connString, function(err, conn){
-    if(err){
-      console.error("Error: ", err);
-      return;
-    }else{
-      var query = "SELECT COMERCIO FROM DASH14467.PROSA GROUP BY COMERCIO";
-      conn.query(query, function(err, rows){
-        if(err){
-          console.log("Error: ", err);
-          return;
-        }else {
-          res.send(rows);
-          conn.close(function(){
-            console.log("Connection closed successfully");
-          });
-        }
-      });
-    }
-  });
-});
+app.get('/api/v1/prosa/comercios', routes.apicall(ibmdb,connString, queries.prosa.comercios));
 
-app.get('/api/v1/prosa/comercios/importe_promedio', function(req, res, next){
-  ibmdb.open(connString, function(err, conn){
-    if(err){
-      console.error("Error: ", err);
-      return;
-    }else{
-      var query = "SELECT COMERCIO, AVG(IMPORTE) AS IMPORTE_PROMEDIO FROM DASH14467.PROSA GROUP BY COMERCIO";
-      conn.query(query, function(err, rows){
-        if(err){
-          console.log("Error: ", err);
-          return;
-        }else {
-          res.send(rows);
-          conn.close(function(){
-            console.log("Connection closed successfully");
-          });
-        }
-      });
-    }
-  });
-});
+app.get('/api/v1/prosa/comercios/importe_promedio', routes.apicall(ibmdb,connString, queries.prosa.comercios_importe_promedio));
 
-app.get('/api/v1/prosa/comercios/resumen', function(req, res, next){
-  ibmdb.open(connString, function(err, conn){
-    if(err){
-      console.error("Error: ", err);
-      return;
-    }else{
-      var query = "SELECT COMERCIO, AVG(IMPORTE) AS IMPORTE_PROMEDIO, SUM(IMPORTE) AS IMPORTE_TOTAL FROM DASH14467.PROSA GROUP BY COMERCIO";
-      conn.query(query, function(err, rows){
-        if(err){
-          console.log("Error: ", err);
-          return;
-        }else {
-          res.send(rows);
-          conn.close(function(){
-            console.log("Connection closed successfully");
-          });
-        }
-      });
-    }
-  });
-});
+app.get('/api/v1/prosa/comercios/resumen', routes.apicall(ibmdb,connString, queries.prosa.comercios_resumen));
 
-app.get('/api/v1/prosa/topten/comercios_mas_ventas', function(req, res, next){
-  ibmdb.open(connString, function(err, conn){
-    if(err){
-      console.error("Error: ", err);
-      return;
-    }else{
-      var query = "SELECT COMERCIO, AVG(IMPORTE) AS IMPORTE_PROMEDIO, SUM(IMPORTE) AS IMPORTE_TOTAL FROM DASH14467.PROSA GROUP BY COMERCIO ORDER BY IMPORTE_TOTAL DESC FETCH FIRST 10 ROW ONLY";
-      conn.query(query, function(err, rows){
-        if(err){
-          console.log("Error: ", err);
-          return;
-        }else {
-          res.send(rows);
-          conn.close(function(){
-            console.log("Connection closed successfully");
-          });
-        }
-      });
-    }
-  });
-});
+app.get('/api/v1/prosa/topten/comercios_mas_ventas', routes.apicall(ibmdb,connString, queries.prosa.comercios_top_ten));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,6 @@ module.exports = {
                 console.error("Error: ", err);
                 return;
             }else{
-                //var query = "SELECT CUSTOMER, COUNT(*) AS TOTAL FROM DASH14467.BLADE_DAMAGE GROUP BY CUSTOMER";
                 conn.query(query, function(err, rows){
                     if(err){
                         console.log("Error: ", err);
@@ -52,39 +51,3 @@ module.exports = {
     }
   }
 }
-
-/*
-exports.listSysTables = function(ibmdb,connString) {
-    return function(req, res) {
-
-
-       ibmdb.open(connString, function(err, conn) {
-			if (err ) {
-			 res.send("error occurred " + err.message);
-			}
-			else {
-				conn.query("SELECT FIRST_NAME, LAST_NAME, EMAIL, WORK_PHONE from GOSALESHR.employee FETCH FIRST 10 ROWS ONLY", function(err, tables, moreResultSets) {
-
-
-				if ( !err ) {
-					res.render('tablelist', {
-						"tablelist" : tables,
-						"tableName" : "10 rows from the GOSALESHR.EMPLOYEE table",
-						"message": "Congratulations. Your connection to dashDB is successful."
-
-					 });
-
-
-				} else {
-				   res.send("error occurred " + err.message);
-				}
-				conn.close(function(){
-					console.log("Connection Closed");
-					});
-				});
-			}
-		} );
-
-	}
-	}
-  */
